935f96299cff5e3a4a3ec01aa341df381c556fcf
MoonLS <moonlightshadow_62@hotmail.com>
2021-04-23 18:03:53 +0200

    Add files via upload

---
+++ export_gps_to_jwg.sh
@@ -0,0 +1,14 @@
+#!
+# Ce script doit permettre d'extraire les donnée EXIF GPS d'une photo et de creer un fichier JWG associé à la photo afin de faciliter l'insertion dans autocad
+
+#Variables
+nom=
+log="./log"
+
+#Fonctions
+while IFS='=' read nom
+do
+	if [ -f ""$nom".JPG" ]
+	then
+		exec exiftool ""$nom".JPG" | grep GPS > ""$nom".JWG"
+	fi
